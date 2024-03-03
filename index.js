bff7835bd18a44d8f02b0a45db425deed8b1f190
maksoon-eu <baranovmaks05@gmail.com>
2024-03-03 13:29:21 +0300

    httpsServer

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        httpsServer.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
         console.log(e)
     }
